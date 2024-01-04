44fad13c40af5b1b9da8daa6f86835941c8832cf
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-01-04 16:11:50 -0600

    .

--- utils/setParams.js
+++ utils/setParams.js
@@ -40,7 +40,7 @@ const setCustomParams = (body) => {
         });
         
         newString = newString.trim();
-        const allSearchs = `(content:*${newString}* OR title:*${newString}* OR subTitle:*${newString}* OR originalAuthor:*${newString}* OR modifierAuthor:*${newString}*)`;
+        const allSearchs = `(content:${newString} OR title:${newString} OR subTitle:${newString} OR originalAuthor:${newString} OR modifierAuthor:${newString})`;
         customQuery = startQuery + allSearchs;
     }
 
