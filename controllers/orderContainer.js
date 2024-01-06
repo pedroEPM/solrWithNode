dfc9c400bdfca3251109b4cc968be98820cc89cf
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-01-06 12:45:37 -0600

    .

--- controllers/orderContainer.js
+++ controllers/orderContainer.js
@@ -71,7 +71,7 @@ class OrderContainer {
                 }
             }
 
-            await removeItemById(1405269, {search: 'Imagenes'});
+            await removeItemById(1398021, {search: 'Imagenes'});
             res.status(200).json({
                 ok: true,
                 msg: 'Added all items'
