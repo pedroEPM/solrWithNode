540ab5fc6d99c7e600dd94b685098efec04dfbf9
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-01-06 12:49:12 -0600

    .

--- controllers/orderContainer.js
+++ controllers/orderContainer.js
@@ -64,7 +64,7 @@ class OrderContainer {
                     // isNewNote.LastModifyDate = new Date(isNewNote.LastModifyDate).toISOString();
 
                     // await addNewItem(isNewNote, {search: 'Notas'});
-                    // await addNewItem(isNewNote, {search: 'Imagenes'});
+                    await addNewItem(isNewNote, {search: 'Imagenes'});
                     // await removeItemById(isNewNote.customId);
 
                 }
